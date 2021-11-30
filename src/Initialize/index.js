6183b37f73a1c93189c287c99089062e77f20815
Craig Wellspring <wellspring.craig@gmail.com>
2021-11-30 00:04:54 -0600

    supabase setup

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../api/auth';
+import Routes from '../routes';
+import SignIn from '../views/SignIn';
 
 function Initialize() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((e, _session) => {
+      setSession(_session);
+    });
+  }, []);
+
   return (
-    <div className="App" />
+    <div className="App">
+      {session ? (<Routes />) : (<SignIn />)}
+    </div>
   );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,11 @@
-// index for router
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Authenticated from '../views/Authenticated';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Authenticated} />
+    </Switch>
+  );
+}
