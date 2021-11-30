9952abff2000f6f668c84d35cdaa827e2079f64e
Craig Wellspring <wellspring.craig@gmail.com>
2021-11-29 20:49:02 -0600

    setup

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,12 +1,18 @@
 import firebase from 'firebase/app';
 
-// These helpers allow you to login and out of FB auth with Google. These are Firebase methods and is broilerplate code.
-
 const signInUser = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider);
 };
+
 const signOutUser = () => new Promise((resolve, reject) => {
   firebase.auth().signOut().then(resolve).catch(reject);
 });
-export { signInUser, signOutUser };
+
+const currentUID = () => firebase.auth().currentUser?.uid;
+
+const currentUser = () => firebase.auth().currentUser;
+
+export {
+  signInUser, signOutUser, currentUID, currentUser,
+};
