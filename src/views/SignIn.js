cdbb2fe1189c1a339a02dccf29cd45080e99aab5
Craig Wellspring <wellspring.craig@gmail.com>
2021-12-21 18:57:49 -0600

    logo fix

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import SignInButton from '../components/buttons/SignInButton';
 import { Panel, PanelTitle } from '../components/StyledComponents';
+import logo from '../reference/favicon.ico';
 
 export default function SignIn() {
   return (
     <Panel>
-      <img width="64" height="64" alt="Site Logo" src="../reference/favicon.ico" />
+      <img width="64" height="64" alt="Site Logo" src={logo} />
       <PanelTitle>Housekeeper</PanelTitle>
       <SignInButton />
     </Panel>
